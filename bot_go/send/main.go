eb9e0378c757678b772a4511f51c25cb627308f5
KAJI <kajiharalivi@icloud.com>
2021-08-13 22:52:21 +0900

    hoge

---
+++ bot_go/send/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/slack-go/slack"
+
+func main() {
+	api := slack.New("xoxb-1491378823348-2286618846368-uIxUrd1gGmuRC2QBPe1euDjr")
+	_, _, _ = api.PostMessage(
+		"bot-sandbox-nomiyama",
+		slack.MsgOptionText("情報セキュリティはやりましたか？モデリングもやりましたか？　のみ", false),
+	)
+}
