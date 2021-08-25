84af24c88e411a07bdd6ee00de4a1d620f7a44e5
kaji <kajiharalivi@icloud.com>
2021-08-25 16:04:13 +0900

    json

--- js/practice.js
+++ js/practice.js
@@ -4,14 +4,14 @@
 // @version      0.1
 // @description  色分け
 // @author       わ　た　し
-// @match        https://gakujo.shizuoka.ac.jp/kyoumu/seisekiStudentSort.do;jsessionid=t-hpPk2yDntBU5zsIc-dTF1cLBp94aOK2EVA1cDF?sortColumn=shuutokuDate
+// @match        https://gakujo.shizuoka.ac.jp/kyoumu/seisekiSearchStudentInit.do;jsessionid=sDcxtgSUsTWQB8wZ4zq25Qwyrhwz2tTAmCMq1vhp?mainMenuCode=008&parentMenuCode=007
 // @icon         https://www.google.com/s2/favicons?domain=shizuoka.ac.jp
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    let rows = document.querySelectorAll("body > table:nth-child(10) > tbody > tr > td > table > tbody > tr")
+     let rows = document.querySelectorAll("body > table:nth-child(10) tr");
     rows = Array.prototype.slice.call(rows,2)
 
     for(let row of rows){
@@ -20,19 +20,19 @@
         row.onmouseout = ""
         switch (grade){
             case "秀":
-                row.style.backgroundColor = "red"
+                row.style.backgroundColor = "red";
                 break;
             case "優":
-                row.style.backgroundColor = "bule"
+                row.style.backgroundColor = "blue";
                 break;
             case "良":
-                row.style.backgroundColor = "yellow"
+                row.style.backgroundColor = "yellow";
                 break;
             case "可":
-                row.style.backgroundColor = "green"
+                row.style.backgroundColor = "green";
                 break;
             case "不可":
-                row.style.backgroundColor = "black"
+                row.style.backgroundColor = "black";
                 break;
             default:
                 console.error(`Unkown grade : ${grade}`);
